8ac7fc1db3d2727ee29216321dad2f589f114478
SadLoneWolf <djordjeandjic99@gmail.com>
2021-11-19 20:18:15 +0300

    added get views for cart

--- ChessShop-master/ChessShop/cart/views.py
+++ ChessShop-master/ChessShop/cart/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from . import models
+from products.models import Chessboard
 # Create your views here.
+def order_index(request,id):
+    order_items = models.OrderItem.objects.get_all()
+    user = models.User.get_by_id(request.id)
+    delivery = models.DeliveryData.objects.get(id=request.id)
+
+def view_item(request,id):
+    board = models.Chessboard.objects.get(id=request.id)
