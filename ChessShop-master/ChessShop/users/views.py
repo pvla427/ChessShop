4b98f93d0dbace8710da8829eec4577f4d2cdcc7
SadLoneWolf <djordjeandjic99@gmail.com>
2021-11-23 14:30:54 +0300

    UserAccount difficulties

--- ChessShop-master/ChessShop/users/views.py
+++ ChessShop-master/ChessShop/users/views.py
@@ -6,3 +6,9 @@ def get_account(request,id):
     user = models.UserAccount.objects.get(id = id)
 
 def register(request):
+    if (request.method == "POST"):
+        user = models.UserAccount()
+        user.save()
+    else:
+         pass
+    
