e9d1bb67e664f7259bf1a3d7439794d585df49b2
SadLoneWolf <djordjeandjic99@gmail.com>
2021-11-23 13:42:27 +0300

    cart GET req. done

--- ChessShop-master/ChessShop/cart/views.py
+++ ChessShop-master/ChessShop/cart/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from . import models
-from products.models import Chessboard
+
 # Create your views here.
 def order_index(request,id):
-    order_items = models.OrderItem.objects.get_all()
+    order_items = models.OrderItem.objects.all()
     user = models.User.get_by_id(request.id)
     delivery = models.DeliveryData.objects.get(id=request.id)
 
 def view_item(request,id):
-    board = models.Chessboard.objects.get(id=request.id)
+   board = models.OrderItem.get(id = id)
+   return board
+
+def view_orders(request):
+    orders = models.Order.objects.all()
+    return orders
+
+def view_delivery(request, id):
+    data = models.DeliveryData.objects.get(id = id)
+    return data
