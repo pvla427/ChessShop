921e964a42321f8c5aaab4892cbb6f41e56ff81d
SadLoneWolf <djordjeandjic99@gmail.com>
2021-12-01 12:46:38 +0300

    Location get

--- ChessShop-master/ChessShop/contacts/views.py
+++ ChessShop-master/ChessShop/contacts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+
+def view_contact(request):
+    contact = models.ContactEntry.objects.get()
+    location = models.Location.objects.get()
+
+def new_contact(request):
+    pass
